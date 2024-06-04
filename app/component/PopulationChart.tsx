fcd5c0d3716aa2dc03d475f3dede67e31c55d89c
Kipeo <kp225048@gmail.com>
2024-06-04 11:44:17 +0900

    first commit: 環境構築 & 都道府県のチェックボックスとグラフのファイル作成

---
+++ app/component/PopulationChart.tsx
@@ -0,0 +1,3 @@
+export default function PopulationChart() {
+  return <div>PopulationChart</div>
+}
---
+++ app/component/PrefectureCheckbox.tsx
@@ -0,0 +1,3 @@
+export default function PrefectureCheckbox() {
+  return <div>都道府県のチェックボックス</div>
+}
